fingul/gevent-websocket

commit 13ea68c0ecde402065b3b98bda545c7ca976dd53

Fixed logging bug

geventwebsocket/handler.py
@@ -241,7 +241,7 @@ class WebSocketHandler(WSGIHandler):
         return self.server.logger
 
     def log_request(self):
-        if '101' not in str(self.status):
+        if b'101' not in str(self.status):
             self.logger.info(self.format_request())
 
     @property

commit d908d7b78045bd90bf334dd6622773fb1f33eda8

Fixed regression in 13ea68c0ecde402065b3b98bda545c7ca976dd53

geventwebsocket/handler.py
@@ -241,7 +241,7 @@ class WebSocketHandler(WSGIHandler):
         return self.server.logger
 
     def log_request(self):
-        if b'101' not in str(self.status):
+        if '101' not in str(self.status):
             self.logger.info(self.format_request())
 
     @property