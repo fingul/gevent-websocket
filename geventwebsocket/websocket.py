fingul/gevent-websocket

commit 17be40b44fd260cbced72db8b1ff5a8776453607

preliminary python3 support

geventwebsocket/websocket.py
@@ -2,10 +2,10 @@ import struct
 
 from socket import error
 
+from ._compat import string_types, range_type, text_type, b
 from .exceptions import ProtocolError
 from .exceptions import WebSocketError
 from .exceptions import FrameTooLargeException
-
 from .utf8validator import Utf8Validator
 
 
@@ -76,13 +76,13 @@ class WebSocket(object):
         :returns: The utf-8 byte string equivalent of `text`.
         """
 
-        if isinstance(text, str):
-            return text
+        if isinstance(text, bytearray):
+            return text.decode('utf-8')
 
-        if not isinstance(text, unicode):
-            text = unicode(text or '')
+        if not isinstance(text, string_types):
+            text = text_type(text or '')
 
-        return text.encode('utf-8')
+        return b(text)
 
     def _is_valid_close_code(self, code):
         """
@@ -166,7 +166,7 @@ class WebSocket(object):
             raise ProtocolError('Invalid close frame: {0} {1}'.format(
                 header, payload))
 
-        code = struct.unpack('!H', str(payload[:2]))[0]
+        code = struct.unpack('!H', payload[:2])[0]
         payload = payload[2:]
 
         if payload:
@@ -203,15 +203,15 @@ class WebSocket(object):
             raise ProtocolError
 
         if not header.length:
-            return header, ''
+            return header, b''
 
         try:
             payload = self.raw_read(header.length)
         except error:
-            payload = ''
+            payload = b''
         except Exception:
             # TODO log out this exception
-            payload = ''
+            payload = b''
 
         if len(payload) != header.length:
             raise WebSocketError('Unexpected EOF reading frame payload')
@@ -238,7 +238,7 @@ class WebSocket(object):
         if an exception is called. Use `receive` instead.
         """
         opcode = None
-        message = ""
+        message = b""
 
         while True:
             header, payload = self.read_frame()
@@ -285,8 +285,8 @@ class WebSocket(object):
                 break
 
         if opcode == self.OPCODE_TEXT:
-            self.validate_utf8(message)
-            return message
+            self.validate_utf8(b(message))
+            return self._decode_bytes(message)
         else:
             return bytearray(message)
 
@@ -323,21 +323,24 @@ class WebSocket(object):
         if opcode == self.OPCODE_TEXT:
             message = self._encode_bytes(message)
         elif opcode == self.OPCODE_BINARY:
-            message = str(message)
+            message = b(str(message))
 
-        header = Header.encode_header(True, opcode, '', len(message), 0)
+        header = Header.encode_header(True, opcode, b'', len(message), 0)
 
         try:
             self.raw_write(header + message)
         except error:
             raise WebSocketError(MSG_SOCKET_DEAD)
+        except:
+            import ipdb; ipdb.set_trace() 
+            raise
 
     def send(self, message, binary=None):
         """
         Send a frame over the websocket with message as its payload
         """
         if binary is None:
-            binary = not isinstance(message, (str, unicode))
+            binary = not isinstance(message, string_types)
 
         opcode = self.OPCODE_BINARY if binary else self.OPCODE_TEXT
 
@@ -347,7 +350,7 @@ class WebSocket(object):
             self.current_app.on_close(MSG_SOCKET_DEAD)
             raise WebSocketError(MSG_SOCKET_DEAD)
 
-    def close(self, code=1000, message=''):
+    def close(self, code=1000, message=b''):
         """
         Close the websocket and connection, sending the specified code and
         message.  The underlying socket object is _not_ closed, that is the
@@ -360,9 +363,7 @@ class WebSocket(object):
         try:
             message = self._encode_bytes(message)
 
-            self.send_frame(
-                struct.pack('!H%ds' % len(message), code, message),
-                opcode=self.OPCODE_CLOSE)
+            self.send_frame(message, opcode=self.OPCODE_CLOSE)
         except WebSocketError:
             # Failed to write the closing frame but it's ok because we're
             # closing the socket anyway.
@@ -420,18 +421,37 @@ class Header(object):
         payload = bytearray(payload)
         mask = bytearray(self.mask)
 
-        for i in xrange(self.length):
+        for i in range_type(self.length):
             payload[i] ^= mask[i % 4]
 
-        return str(payload)
+        return payload
 
     # it's the same operation
     unmask_payload = mask_payload
 
     def __repr__(self):
-        return ("<Header fin={0} opcode={1} length={2} flags={3} at "
-                "0x{4:x}>").format(self.fin, self.opcode, self.length,
-                                   self.flags, id(self))
+        opcodes = {
+            0: 'continuation(0)',
+            1: 'text(1)',
+            2: 'binary(2)',
+            8: 'close(8)',
+            9: 'ping(9)',
+            10: 'pong(10)'
+        }
+        flags = {
+            0x40: 'RSV1 MASK',
+            0x20: 'RSV2 MASK',
+            0x10: 'RSV3 MASK'
+        }
+
+        return ("<Header fin={0} opcode={1} length={2} flags={3} mask={4} at "
+                "0x{5:x}>").format(
+                    self.fin,
+                    opcodes.get(self.opcode, 'reserved({})'.format(self.opcode)),
+                    self.length,
+                    flags.get(self.flags, 'reserved({})'.format(self.flags)),
+                    self.mask, id(self)
+        )
 
     @classmethod
     def decode_header(cls, stream):
@@ -509,7 +529,7 @@ class Header(object):
         """
         first_byte = opcode
         second_byte = 0
-        extra = ''
+        extra = None
 
         if fin:
             first_byte |= cls.FIN_MASK
@@ -540,4 +560,9 @@ class Header(object):
 
             extra += mask
 
-        return chr(first_byte) + chr(second_byte) + extra
+        if extra:
+            header = (first_byte, second_byte, extra)
+        else:
+            header = (first_byte, second_byte)
+
+        return bytes(bytearray(header))

examples/chat/chat.py
@@ -1,9 +1,11 @@
+from __future__ import print_function
+
 import json
 
 from gevent import monkey
 monkey.patch_all()
 
-from flask import Flask, app, render_template
+from flask import Flask, render_template
 from werkzeug.debug import DebuggedApplication
 
 from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
@@ -14,7 +16,7 @@ flask_app.debug = True
 
 class ChatApplication(WebSocketApplication):
     def on_open(self):
-        print "Some client connected!"
+        print("Some client connected!")
 
     def on_message(self, message):
         if message is None:
@@ -48,7 +50,7 @@ class ChatApplication(WebSocketApplication):
             }))
 
     def on_close(self, reason):
-        print "Connection closed! "
+        print("Connection closed!")
 
 
 @flask_app.route('/')

examples/echoserver.py
@@ -1,9 +1,9 @@
-import os
+from __future__ import print_function
 
+import os
 import geventwebsocket
 
 from geventwebsocket.server import WebSocketServer
-
 
 
 def echo_app(environ, start_response):
@@ -16,8 +16,8 @@ def echo_app(environ, start_response):
             message = websocket.receive()
             websocket.send(message)
         websocket.close()
-    except geventwebsocket.WebSocketError, ex:
-        print "{0}: {1}".format(ex.__class__.__name__, ex)
+    except geventwebsocket.WebSocketError as ex:
+        print("{0}: {1}".format(ex.__class__.__name__, ex))
 
 
 def http_handler(environ, start_response):
@@ -32,7 +32,8 @@ def http_handler(environ, start_response):
 
 
 path = os.path.dirname(geventwebsocket.__file__)
-agent = "gevent-websocket/%s" % (geventwebsocket.get_version())
+agent = bytearray("gevent-websocket/%s" % (geventwebsocket.get_version()),
+                  'latin-1')
 
-print "Running %s from %s" % (agent, path)
+print("Running %s from %s" % (agent, path))
 WebSocketServer(("", 8000), echo_app, debug=False).serve_forever()

examples/plot_graph.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 """
 This example generates random data and plots a graph in the browser.
 
@@ -9,21 +11,21 @@ Or with an Gunicorn wrapper:
         plot_graph:resource
 """
 
-
 import gevent
 import random
 
 from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
+from geventwebsocket._compat import range_type
 
 
 class PlotApplication(WebSocketApplication):
     def on_open(self):
-        for i in xrange(10000):
+        for i in range_type(10000):
             self.ws.send("0 %s %s\n" % (i, random.random()))
             gevent.sleep(0.1)
 
     def on_close(self, reason):
-        print "Connection Closed!!!", reason
+        print("Connection Closed!!!", reason)
 
 
 def static_wsgi_app(environ, start_response):

geventwebsocket/_compat.py
@@ -1,0 +1,23 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+import codecs
+
+
+PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2
+
+
+if PY2:
+    bytes = str
+    text_type = unicode
+    string_types = (str, unicode)
+    range_type = xrange
+    iteritems = lambda x: x.iteritems()
+    b = lambda x: x
+else:
+    text_type = str
+    string_types = str,
+    range_type = range
+    iteritems = lambda x: iter(x.items())
+    b = lambda x: codecs.latin_1_encode(x)[0]

geventwebsocket/handler.py
@@ -1,8 +1,8 @@
 import base64
 import hashlib
-import warnings
 
 from gevent.pywsgi import WSGIHandler
+from ._compat import PY3
 from .websocket import WebSocket, Stream
 from .logging import create_logger
 
@@ -212,11 +212,16 @@ class WebSocketHandler(WSGIHandler):
             'wsgi.websocket': self.websocket
         })
 
+        if PY3:
+            accept = base64.b64encode(
+                hashlib.sha1((key + self.GUID).encode()).digest()).decode()
+        else:
+            accept = base64.b64encode(hashlib.sha1(key + self.GUID).digest())
+
         headers = [
             ("Upgrade", "websocket"),
             ("Connection", "Upgrade"),
-            ("Sec-WebSocket-Accept", base64.b64encode(
-                hashlib.sha1(key + self.GUID).digest())),
+            ("Sec-WebSocket-Accept", accept)
         ]
 
         if protocol:
@@ -233,7 +238,7 @@ class WebSocketHandler(WSGIHandler):
         return self.server.logger
 
     def log_request(self):
-        if '101' not in self.status:
+        if '101' not in str(self.status):
             self.logger.info(self.format_request())
 
     @property

geventwebsocket/protocols/wamp.py
@@ -11,6 +11,7 @@ except ImportError:
     except ImportError:
         import json
 
+from .._compat import range_type, string_types
 from ..exceptions import WebSocketError
 from .base import BaseProtocol
 
@@ -131,7 +132,7 @@ class WampProtocol(BaseProtocol):
         self.prefixes = Prefixes()
         self.session_id = ''.join(
             [random.choice(string.digits + string.letters)
-                for i in xrange(16)])
+                for i in range_type(16)])
 
         super(WampProtocol, self).__init__(*args, **kwargs)
 
@@ -168,9 +169,9 @@ class WampProtocol(BaseProtocol):
         call_id, curie_or_uri = data[1:3]
         args = data[3:]
 
-        if not isinstance(call_id, (str, unicode)):
+        if not isinstance(call_id, string_types):
             raise Exception()
-        if not isinstance(curie_or_uri, (str, unicode)):
+        if not isinstance(curie_or_uri, string_types):
             raise Exception()
 
         uri = self.prefixes.resolve(curie_or_uri)
@@ -178,7 +179,7 @@ class WampProtocol(BaseProtocol):
         try:
             result = self.procedures.call(uri, args)
             result_msg = [self.MSG_CALL_RESULT, call_id, result]
-        except Exception, e:
+        except Exception as e:
             result_msg = [self.MSG_CALL_ERROR,
                           call_id] + self._get_exception_info(e)
 
@@ -190,7 +191,7 @@ class WampProtocol(BaseProtocol):
 
         if not isinstance(action, int):
             raise Exception()
-        if not isinstance(curie_or_uri, (str, unicode)):
+        if not isinstance(curie_or_uri, string_types):
             raise Exception()
 
         uri = self.prefixes.resolve(curie_or_uri)

geventwebsocket/utf8validator.py
@@ -1,128 +1,221 @@
-###############################################################################
-##
-##  Copyright 2011-2013 Tavendo GmbH
-##
-##  Note:
-##
-##  This code is a Python implementation of the algorithm
-##
-##            "Flexible and Economical UTF-8 Decoder"
-##
-##  by Bjoern Hoehrmann
-##
-##       bjoern@hoehrmann.de
-##       http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
-##
-##  Licensed under the Apache License, Version 2.0 (the "License");
-##  you may not use this file except in compliance with the License.
-##  You may obtain a copy of the License at
-##
-##      http://www.apache.org/licenses/LICENSE-2.0
-##
-##  Unless required by applicable law or agreed to in writing, software
-##  distributed under the License is distributed on an "AS IS" BASIS,
-##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-##  See the License for the specific language governing permissions and
-##  limitations under the License.
-##
-###############################################################################
+from ._compat import PY3
 
 
-## use Cython implementation of UTF8 validator if available
-##
+###############################################################################
+#
+# The MIT License (MIT)
+#
+# Copyright (c) Tavendo GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+###############################################################################
+
+# Note: This code is a Python implementation of the algorithm
+# "Flexible and Economical UTF-8 Decoder" by Bjoern Hoehrmann
+# bjoern@hoehrmann.de, http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
+
+__all__ = ("Utf8Validator",)
+
+
+# DFA transitions
+UTF8VALIDATOR_DFA = (
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 00..1f
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 20..3f
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 40..5f
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 60..7f
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,  # 80..9f
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,  # a0..bf
+    8, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,  # c0..df
+    0xa, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x3, 0x4, 0x3, 0x3,  # e0..ef
+    0xb, 0x6, 0x6, 0x6, 0x5, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8, 0x8,  # f0..ff
+    0x0, 0x1, 0x2, 0x3, 0x5, 0x8, 0x7, 0x1, 0x1, 0x1, 0x4, 0x6, 0x1, 0x1, 0x1, 0x1,  # s0..s0
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1,  # s1..s2
+    1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1,  # s3..s4
+    1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 3, 1, 1, 1, 1, 1, 1,  # s5..s6
+    1, 3, 1, 1, 1, 1, 1, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  # s7..s8
+)
+
+UTF8_ACCEPT = 0
+UTF8_REJECT = 1
+
+
+# use Cython implementation of UTF8 validator if available
+#
 try:
     from wsaccel.utf8validator import Utf8Validator
-except:
-    ## fallback to pure Python implementation
 
-    class Utf8Validator:
-        """
-        Incremental UTF-8 validator with constant memory consumption (minimal
-        state).
+except ImportError:
+    #
+    # Fallback to pure Python implementation - also for PyPy.
+    #
+    # Do NOT touch this code unless you know what you are doing!
+    # https://github.com/oberstet/scratchbox/tree/master/python/utf8
+    #
 
-        Implements the algorithm "Flexible and Economical UTF-8 Decoder" by
-        Bjoern Hoehrmann (http://bjoern.hoehrmann.de/utf-8/decoder/dfa/).
-        """
+    if PY3:
+        # Python 3 and above
 
-        ## DFA transitions
-        UTF8VALIDATOR_DFA = [
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  # 00..1f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  # 20..3f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  # 40..5f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  # 60..7f
-            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,  # 80..9f
-            7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  # a0..bf
-            8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  # c0..df
-            0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3,  # e0..ef
-            0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,  # f0..ff
-            0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1,  # s0..s0
-            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1,  # s1..s2
-            1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,  # s3..s4
-            1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,  # s5..s6
-            1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  # s7..s8
-        ]
+        # convert DFA table to bytes (performance)
+        UTF8VALIDATOR_DFA_S = bytes(UTF8VALIDATOR_DFA)
 
-        UTF8_ACCEPT = 0
-        UTF8_REJECT = 1
-
-        def __init__(self):
-            self.reset()
-
-        def decode(self, b):
+        class Utf8Validator(object):
             """
-            Eat one UTF-8 octet, and validate on the fly.
+            Incremental UTF-8 validator with constant memory consumption (minimal state).
 
-            Returns UTF8_ACCEPT when enough octets have been consumed, in which case
-            self.codepoint contains the decoded Unicode code point.
-
-            Returns UTF8_REJECT when invalid UTF-8 was encountered.
-
-            Returns some other positive integer when more octets need to be eaten.
-            """
-            type = Utf8Validator.UTF8VALIDATOR_DFA[b]
-
-            if self.state != Utf8Validator.UTF8_ACCEPT:
-                self.codepoint = (b & 0x3f) | (self.codepoint << 6)
-            else:
-                self.codepoint = (0xff >> type) & b
-
-            self.state = Utf8Validator.UTF8VALIDATOR_DFA[256 + self.state * 16 + type]
-
-            return self.state
-
-        def reset(self):
-            """
-            Reset validator to start new incremental UTF-8 decode/validation.
-            """
-            self.state = Utf8Validator.UTF8_ACCEPT
-            self.codepoint = 0
-            self.i = 0
-
-        def validate(self, ba):
-            """
-            Incrementally validate a chunk of bytes provided as string.
-
-            Will return a quad (valid?, endsOnCodePoint?, currentIndex, totalIndex).
-
-            As soon as an octet is encountered which renders the octet sequence
-            invalid, a quad with valid? == False is returned. currentIndex returns
-            the index within the currently consumed chunk, and totalIndex the
-            index within the total consumed sequence that was the point of bail out.
-            When valid? == True, currentIndex will be len(ba) and totalIndex the
-            total amount of consumed bytes.
+            Implements the algorithm "Flexible and Economical UTF-8 Decoder" by
+            Bjoern Hoehrmann (http://bjoern.hoehrmann.de/utf-8/decoder/dfa/).
             """
 
-            l = len(ba)
+            def __init__(self):
+                self.reset()
 
-            for i in xrange(l):
-                ## optimized version of decode(), since we are not interested in actual code points
+            def decode(self, b):
+                """
+                Eat one UTF-8 octet, and validate on the fly.
 
-                self.state = Utf8Validator.UTF8VALIDATOR_DFA[256 + (self.state << 4) + Utf8Validator.UTF8VALIDATOR_DFA[ord(ba[i])]]
+                Returns ``UTF8_ACCEPT`` when enough octets have been consumed, in which case
+                ``self.codepoint`` contains the decoded Unicode code point.
 
-                if self.state == Utf8Validator.UTF8_REJECT:
-                    self.i += i
-                    return False, False, i, self.i
+                Returns ``UTF8_REJECT`` when invalid UTF-8 was encountered.
 
-            self.i += l
+                Returns some other positive integer when more octets need to be eaten.
+                """
+                tt = UTF8VALIDATOR_DFA_S[b]
+                if self.state != UTF8_ACCEPT:
+                    self.codepoint = (b & 0x3f) | (self.codepoint << 6)
+                else:
+                    self.codepoint = (0xff >> tt) & b
+                self.state = UTF8VALIDATOR_DFA_S[256 + self.state * 16 + tt]
+                return self.state
 
-            return True, self.state == Utf8Validator.UTF8_ACCEPT, l, self.i
+            def reset(self):
+                """
+                Reset validator to start new incremental UTF-8 decode/validation.
+                """
+                self.state = UTF8_ACCEPT  # the empty string is valid UTF8
+                self.codepoint = 0
+                self.i = 0
+
+            def validate(self, ba):
+                """
+                Incrementally validate a chunk of bytes provided as string.
+
+                Will return a quad ``(valid?, endsOnCodePoint?, currentIndex, totalIndex)``.
+
+                As soon as an octet is encountered which renders the octet sequence
+                invalid, a quad with ``valid? == False`` is returned. ``currentIndex`` returns
+                the index within the currently consumed chunk, and ``totalIndex`` the
+                index within the total consumed sequence that was the point of bail out.
+                When ``valid? == True``, currentIndex will be ``len(ba)`` and ``totalIndex`` the
+                total amount of consumed bytes.
+                """
+                #
+                # The code here is written for optimal JITting in PyPy, not for best
+                # readability by your grandma or particular elegance. Do NOT touch!
+                #
+                l = len(ba)
+                i = 0
+                state = self.state
+                while i < l:
+                    # optimized version of decode(), since we are not interested in actual code points
+                    state = UTF8VALIDATOR_DFA_S[256 + (state << 4) + UTF8VALIDATOR_DFA_S[ba[i]]]
+                    if state == UTF8_REJECT:
+                        self.state = state
+                        self.i += i
+                        return False, False, i, self.i
+                    i += 1
+                self.state = state
+                self.i += l
+                return True, state == UTF8_ACCEPT, l, self.i
+
+    else:
+
+        # convert DFA table to string (performance)
+        UTF8VALIDATOR_DFA_S = ''.join([chr(c) for c in UTF8VALIDATOR_DFA])
+
+        class Utf8Validator(object):
+            """
+            Incremental UTF-8 validator with constant memory consumption (minimal state).
+
+            Implements the algorithm "Flexible and Economical UTF-8 Decoder" by
+            Bjoern Hoehrmann (http://bjoern.hoehrmann.de/utf-8/decoder/dfa/).
+            """
+
+            def __init__(self):
+                self.reset()
+
+            def decode(self, b):
+                """
+                Eat one UTF-8 octet, and validate on the fly.
+
+                Returns ``UTF8_ACCEPT`` when enough octets have been consumed, in which case
+                ``self.codepoint`` contains the decoded Unicode code point.
+
+                Returns ``UTF8_REJECT`` when invalid UTF-8 was encountered.
+
+                Returns some other positive integer when more octets need to be eaten.
+                """
+                tt = ord(UTF8VALIDATOR_DFA_S[b])
+                if self.state != UTF8_ACCEPT:
+                    self.codepoint = (b & 0x3f) | (self.codepoint << 6)
+                else:
+                    self.codepoint = (0xff >> tt) & b
+                self.state = ord(UTF8VALIDATOR_DFA_S[256 + self.state * 16 + tt])
+                return self.state
+
+            def reset(self):
+                """
+                Reset validator to start new incremental UTF-8 decode/validation.
+                """
+                self.state = UTF8_ACCEPT  # the empty string is valid UTF8
+                self.codepoint = 0
+                self.i = 0
+
+            def validate(self, ba):
+                """
+                Incrementally validate a chunk of bytes provided as string.
+
+                Will return a quad ``(valid?, endsOnCodePoint?, currentIndex, totalIndex)``.
+
+                As soon as an octet is encountered which renders the octet sequence
+                invalid, a quad with ``valid? == False`` is returned. ``currentIndex`` returns
+                the index within the currently consumed chunk, and ``totalIndex`` the
+                index within the total consumed sequence that was the point of bail out.
+                When ``valid? == True``, currentIndex will be ``len(ba)`` and ``totalIndex`` the
+                total amount of consumed bytes.
+                """
+                #
+                # The code here is written for optimal JITting in PyPy, not for best
+                # readability by your grandma or particular elegance. Do NOT touch!
+                #
+                l = len(ba)
+                i = 0
+                state = self.state
+                while i < l:
+                    # optimized version of decode(), since we are not interested in actual code points
+                    state = ord(UTF8VALIDATOR_DFA_S[256 + (state << 4) + ord(UTF8VALIDATOR_DFA_S[ord(ba[i])])])
+                    if state == UTF8_REJECT:
+                        self.state = state
+                        self.i += i
+                        return False, False, i, self.i
+                    i += 1
+                self.state = state
+                self.i += l
+                return True, state == UTF8_ACCEPT, l, self.i

commit d9b1a639eafead5b69a543b866bf5a7f81a8f0d4

Fixed regression in 17be40b44fd260cbced72db8b1ff5a8776453607

geventwebsocket/websocket.py
@@ -529,7 +529,7 @@ class Header(object):
         """
         first_byte = opcode
         second_byte = 0
-        extra = None
+        extra = b''
 
         if fin:
             first_byte |= cls.FIN_MASK